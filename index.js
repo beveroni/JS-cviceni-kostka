7b05ef6f9af7ad998033d508795ea7ef1077b41d
Veronika Bendova <svechovaver@gmail.com>
2023-07-29 19:07:11 +0200

    Task done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('funguju!');
+
+let aktualniCislo = 1;
+const kostka = document.querySelector('.dice');
+const otocKostku = () => {
+  aktualniCislo = aktualniCislo + 1;
+  if (aktualniCislo === 7) {
+    aktualniCislo = 1;
+  }
+  kostka.src = `img/side${aktualniCislo}.svg`;
+};
+document.addEventListener('click', otocKostku);
